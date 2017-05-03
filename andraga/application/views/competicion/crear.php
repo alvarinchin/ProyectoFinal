29d7d0c3c97746000996d8bba37102dca2cc9c42
barbara <barbara.jurado.daw@gmail.com>
2017-05-03 12:35:51 +0200

    Modificado formulario creación de competiciones incluyendo un campo de fecha de la competicion

--- andraga/application/views/competicion/crear.php
+++ andraga/application/views/competicion/crear.php
@@ -1,10 +1,11 @@
 <div class="container">
 	<h4>Introduce los datos de nueva competición</h4>
-
-	<label for="nombre">Nombre: </label> <input value="" type="text"
-		id="nombre" placeholder="Nombre de la competición" required>
-	<!--<input type="submit" value="Enviar">-->
-	<input type="button" value="Enviar">
-
+	<form method="post">
+		<label for="idNombre">Nombre: </label> <input value="" type="text"
+			id="idNombre" placeholder="Nombre de la competición" required> <br/>
+		<label for="idFecha">Fecha: </label> <input type="date" id="idFecha"> 
+		
+		<input type="button" value="Siguiente">
+	</form>
 </div>
 
--- andraga/application/views/templates/Template_main.php
+++ andraga/application/views/templates/Template_main.php
@@ -1,8 +1,8 @@
 <?php $this->load->view("templates/head");?>
 <?php 
-if (!empty($_REQUEST["_activo"])){
+
 	$this->load->view("templates/nav");
-}
+
 ?>	
 <div id="contents"><?= $contents ?></div>
 <?php $this->load->view("templates/footer");?>

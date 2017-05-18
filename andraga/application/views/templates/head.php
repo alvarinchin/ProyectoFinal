fc9f00496a0c7895bab4c4a7355bac8b1ffe6d67
barbara <barbara@pc-barbara>
2017-05-18 21:23:59 +0200

    Agregadas librerías y css de jqueryUI

--- andraga/application/views/templates/head.php
+++ andraga/application/views/templates/head.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html >
-<html ng-app="administracion" ng-controller="mainCtrl" >
+<html ng-app="administracion" ng-controller="mainCtrl">
 <head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link href="<?=base_url()?>/assets/bootstrap/bootstrap.min.css" rel="stylesheet">
+<link href="<?=base_url()?>/assets/bootstrap/bootstrap.min.css"
+	rel="stylesheet">
 <link href="<?=base_url()?>/assets/bootstrap/bootstrap-theme.min.css"
+	rel="stylesheet">
+<script type="text/javascript"
+	src="<?=base_url()?>/assets/jquery/jquery.js"></script>
+<script type="text/javascript"
+	src="<?=base_url()?>/assets/jqueryui/jquery-ui.min.js"></script>
+<link href="<?=base_url()?>/assets/jqueryui/jquery-ui.min.css"
 	rel="stylesheet">
 
 <title>Gestor Competiciones</title>

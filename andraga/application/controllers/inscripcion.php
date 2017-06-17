a323959e496df4a37ef00b3800d457dc5e4f67bd
Marcidus <Marcidus@192.168.1.128>
2017-06-17 19:19:26 +0200

    Securizado Rotación e inscripción

--- andraga/application/controllers/inscripcion.php
+++ andraga/application/controllers/inscripcion.php
@@ -1,54 +1,49 @@
 <?php
 require_once 'vendor/autoload.php';
 require_once 'JwtController.php';
-    
 class inscripcion extends JwtController {
 	public function __construct() {
 		parent::__construct ();
 		$this->load->model ( "adaptador_model" );
 	}
 	public function insertar() {
-        
-            if ($this->consultarPermisosJuez()) {
-		if (isset ( $_REQUEST ["idClub"] )) {
-			if (! empty ( $_REQUEST ["idClub"] ) && ! empty ( $_REQUEST ["idDeportistas"] ) && ! empty ( $_REQUEST ["idCompeticion"] ) && ! empty ( $_REQUEST ["idCategoria"] ) && ! empty ( $_REQUEST ["idEspecialidad"] )&& ! empty ( $_REQUEST ["idTipoejErcicio"] )) {
-                            
-				$club = $this->adaptador_model->getOne ( "club", $this->utilphp->sanear ( $_REQUEST ["idClub"] ) );
-				$competicion = $this->adaptador_model->getOne ( "competicion", $this->utilphp->sanear ( $_REQUEST ["idCompeticion"] ) );
-				$especialidad = $this->adaptador_model->getOne ( "especialidad", $this->utilphp->sanear ( $_REQUEST ["idEspecialidad"] ) );
-				$categoria = $this->adaptador_model->getOne ( "categoria", $this->utilphp->sanear ( $_REQUEST ["idCategoria"] ) );
-				$tipoejercicio = $this->adaptador_model->getOne ( "tipoejercicio", $this->utilphp->sanear ( $_REQUEST ["idTipoejErcicio"] ) );
-                                // $campos ["dorsal"]= strtoupper(substr($campos["Club"]["nombre"],0,2)).rand(1, 90);
-				// $dorsal = $this->utilphp->sanear ( $_REQUEST ["dorsal"] );
-                                
-                                
-				$deportistas = $this->cargarDeportistas ( $this->utilphp->sanear ( $_REQUEST ["idDeportistas"] ) );
-				// $deportistas=$this->adaptador_model->getOne("deportista",$this->utilphp->sanear ($_REQUEST ["idDeportistas"]));
-                                    
-				try {
-					$this->load->model ( "inscripcion_model" );
-					$numBeansInscripcion = $this->adaptador_model->count ( "inscripcion" );
-				} catch ( Exception $e ) {
-				}
-				if ($numBeansInscripcion != 0) {
-					$dorsal = $numBeansInscripcion + 1;
-					$status = $this->inscripcion_model->insert ( $club, $competicion, $categoria, $especialidad, $deportistas, $dorsal,$tipoejercicio );
-				} else {
-					$this->load->model ( "inscripcion_model" );
-					// $numBeansInscripcion = $this->adaptador_model->count ( "inscripcion" );
-					$dorsal = 1;
-					$status = $this->inscripcion_model->insert ( $club, $competicion, $categoria, $especialidad, $deportistas, $dorsal,$tipoejercicio  );
-				}
-                                    
-				if ($status) {
-					echo json_encode ( array (
-							"status" => "ok",
-							"data" => $_REQUEST,
-							"msg" => "Inserción correcta",
-							"debub" => $status 
-					) );
-                                            
-                                            
+		if ($this->consultarPermisosJuez ()) {
+			if (isset ( $_REQUEST ["idClub"] )) {
+				if (! empty ( $_REQUEST ["idClub"] ) && ! empty ( $_REQUEST ["idDeportistas"] ) && ! empty ( $_REQUEST ["idCompeticion"] ) && ! empty ( $_REQUEST ["idCategoria"] ) && ! empty ( $_REQUEST ["idEspecialidad"] ) && ! empty ( $_REQUEST ["idTipoejErcicio"] )) {
+					
+					$club = $this->adaptador_model->getOne ( "club", $this->utilphp->sanear ( $_REQUEST ["idClub"] ) );
+					$competicion = $this->adaptador_model->getOne ( "competicion", $this->utilphp->sanear ( $_REQUEST ["idCompeticion"] ) );
+					$especialidad = $this->adaptador_model->getOne ( "especialidad", $this->utilphp->sanear ( $_REQUEST ["idEspecialidad"] ) );
+					$categoria = $this->adaptador_model->getOne ( "categoria", $this->utilphp->sanear ( $_REQUEST ["idCategoria"] ) );
+					$tipoejercicio = $this->adaptador_model->getOne ( "tipoejercicio", $this->utilphp->sanear ( $_REQUEST ["idTipoejErcicio"] ) );
+					// $campos ["dorsal"]= strtoupper(substr($campos["Club"]["nombre"],0,2)).rand(1, 90);
+					// $dorsal = $this->utilphp->sanear ( $_REQUEST ["dorsal"] );
+					
+					$deportistas = $this->cargarDeportistas ( $this->utilphp->sanear ( $_REQUEST ["idDeportistas"] ) );
+					// $deportistas=$this->adaptador_model->getOne("deportista",$this->utilphp->sanear ($_REQUEST ["idDeportistas"]));
+					
+					try {
+						$this->load->model ( "inscripcion_model" );
+						$numBeansInscripcion = $this->adaptador_model->count ( "inscripcion" );
+					} catch ( Exception $e ) {
+					}
+					if ($numBeansInscripcion != 0) {
+						$dorsal = $numBeansInscripcion + 1;
+						$status = $this->inscripcion_model->insert ( $club, $competicion, $categoria, $especialidad, $deportistas, $dorsal, $tipoejercicio );
+					} else {
+						$this->load->model ( "inscripcion_model" );
+						// $numBeansInscripcion = $this->adaptador_model->count ( "inscripcion" );
+						$dorsal = 1;
+						$status = $this->inscripcion_model->insert ( $club, $competicion, $categoria, $especialidad, $deportistas, $dorsal, $tipoejercicio );
+					}
+					
+					if ($status) {
+						echo json_encode ( array (
+								"status" => "ok",
+								"data" => $_REQUEST,
+								"msg" => "Inserción correcta",
+								"debub" => $status 
+						) );
 					} else {
 						echo json_encode ( array (
 								"status" => "error",
@@ -68,51 +63,46 @@ class inscripcion extends JwtController {
 						"msg" => "Error no han llegado los datos" 
 				) );
 			}
-            }
 		}
-                    
+	}
 	private function cargarDeportistas($ids) {
 		$deportistas = [ ];
 		$ids = explode ( ",", $ids );
 		foreach ( $ids as $key => $value ) {
-                    
+			
 			$deportistas [$key] = $this->adaptador_model->getOne ( "deportista", $value );
 		}
-                    
+		
 		return $deportistas;
 	}
 	public function listar() {
-            if ($this->consultarPermisosJuez()) {
-        
-		$campos = [ 
-				"club",
-				"categoria",
-				"especialidad",
-				"competicion",
-				"dorsal",
-				"ownDeportistaList" ,
-                                "tipoejercicio"
-		];
-		$res = [ ];
-                
-		$inscripciones = $this->adaptador_model->getAll ( "inscripcion" );
-
-   
-                
-		foreach ( $inscripciones as $k => $inscripcion ) {
-			$fila = [ ];
-			$ins = $this->adaptador_model->getOne ( "inscripcion", $inscripcion->id );
-                        $fila["id"]=$inscripcion->id ;
-			foreach ( $campos as $ke => $campo ) {
-                            
-				$fila [$campo] = $ins->$campo;
-                                    
+		if ($this->consultarListar ()) {
+			
+			$campos = [ 
+					"club",
+					"categoria",
+					"especialidad",
+					"competicion",
+					"dorsal",
+					"ownDeportistaList",
+					"tipoejercicio" 
+			];
+			$res = [ ];
+			
+			$inscripciones = $this->adaptador_model->getAll ( "inscripcion" );
+			
+			foreach ( $inscripciones as $k => $inscripcion ) {
+				$fila = [ ];
+				$ins = $this->adaptador_model->getOne ( "inscripcion", $inscripcion->id );
+				$fila ["id"] = $inscripcion->id;
+				foreach ( $campos as $ke => $campo ) {
+					
+					$fila [$campo] = $ins->$campo;
+				}
 				
+				$res [$k] = $fila;
 			}
-                        
-                        $res [$k] = $fila;
-                }
-                         
+			
 			if ($res != null) {
 				// deben devolverse en un echo porque son cadenas de texto
 				echo json_encode ( array (
@@ -125,18 +115,16 @@ class inscripcion extends JwtController {
 						"status" => "error",
 						"msg" => "error en BD" 
 				) );
-                                    
 			}
 		}
-        
 	}
 	public function borrar() {
-		if ($this->consultarPermisosJuez()) {
+		if ($this->consultarPermisosJuez ()) {
 			if (isset ( $_REQUEST ["inscSel"] )) {
 				if (! empty ( $_REQUEST ["inscSel"] )) {
-                                    
+					
 					$status = $this->adaptador_model->delete ( "inscripcion", $this->utilphp->sanear ( $_REQUEST ["inscSel"] ) );
-                                            
+					
 					if ($status) {
 						echo json_encode ( array (
 								"status" => "ok",
--- andraga/application/controllers/rotacion.php
+++ andraga/application/controllers/rotacion.php
@@ -6,43 +6,45 @@ class rotacion extends JwtController {
 	public function __construct() {
 		parent::__construct ();
 		$this->load->model ( "adaptador_model" );
-                $this->load->model ( "rotacion_model" );
+		$this->load->model ( "rotacion_model" );
 	}
 	public function insertar() {
-		if (isset ( $_REQUEST ["id"] )) {
-			if (! empty ( $_REQUEST ["id"] )) {
-				
-				$inscripcion = $this->adaptador_model->getOne ( "inscripcion", $this->utilphp->sanear ( $_REQUEST ["id"] ) );
-				//que se cree y vaya creciendo 
-                                $orden=0;
-                                $puntuacion=null;
-				$status = $this->rotacion_model->insert ( $inscripcion , $orden, $puntuacion);
-				
-				if ($status) {
-					echo json_encode ( array (
-							"status" => "ok",
-							"data" => $_REQUEST,
-							"msg" => "Inserción correcta",
-							"debub" => $status
-					) );
+		if ($this->consultarPermisosJuez ()) {
+			if (isset ( $_REQUEST ["id"] )) {
+				if (! empty ( $_REQUEST ["id"] )) {
+					
+					$inscripcion = $this->adaptador_model->getOne ( "inscripcion", $this->utilphp->sanear ( $_REQUEST ["id"] ) );
+					// que se cree y vaya creciendo
+					$orden = 0;
+					$puntuacion = null;
+					$status = $this->rotacion_model->insert ( $inscripcion, $orden, $puntuacion );
+					
+					if ($status) {
+						echo json_encode ( array (
+								"status" => "ok",
+								"data" => $_REQUEST,
+								"msg" => "Inserción correcta",
+								"debub" => $status 
+						) );
+					} else {
+						echo json_encode ( array (
+								"status" => "error",
+								"msg" => "Error al insertar rotación nueva, nombre repetido",
+								"debub" => $status 
+						) );
+					}
 				} else {
 					echo json_encode ( array (
 							"status" => "error",
-							"msg" => "Error al insertar rotación nueva, nombre repetido",
-							"debub" => $status
+							"msg" => "Error algún dato está vacío" 
 					) );
 				}
 			} else {
 				echo json_encode ( array (
 						"status" => "error",
-						"msg" => "Error algún dato está vacío"
+						"msg" => "Error no han llegado los datos" 
 				) );
 			}
-		} else {
-			echo json_encode ( array (
-					"status" => "error",
-					"msg" => "Error no han llegado los datos"
-			) );
 		}
 	}
 	private function cargarDeportistas($ids) {
@@ -56,70 +58,74 @@ class rotacion extends JwtController {
 		return $deportistas;
 	}
 	public function listar() {
-		$campos = [
-				"categoria",
-				"especialidad",
-				"dorsal",
-				"ownDeportistaList",
-                    "tipoejercicio"
-				
-		];
-		$res = [ ];
-		$rotaciones = $this->adaptador_model->getAll ( "rotacion" );
-		foreach ( $rotaciones as $k => $rotacion ) {
-			$fila = [ ];
-			$rot = ($this->adaptador_model->getOne ( "rotacion", $rotacion->id ));
-                        $ins= $rot->inscripcion;
-			foreach ( $campos as $ke => $campo ) {
-				$fila [$campo] = $ins->$campo;
+		if ($this->consultarListar ()) {
+			$campos = [ 
+					"categoria",
+					"especialidad",
+					"dorsal",
+					"ownDeportistaList",
+					"tipoejercicio" 
+			
+			];
+			$res = [ ];
+			$rotaciones = $this->adaptador_model->getAll ( "rotacion" );
+			foreach ( $rotaciones as $k => $rotacion ) {
+				$fila = [ ];
+				$rot = ($this->adaptador_model->getOne ( "rotacion", $rotacion->id ));
+				$ins = $rot->inscripcion;
+				foreach ( $campos as $ke => $campo ) {
+					$fila [$campo] = $ins->$campo;
+				}
+				$fila ["id"] = $rotacion->id;
+				$fila ["orden"] = $rotacion->orden;
+				$res [$k] = $fila;
 			}
-			$fila ["id"] = $rotacion->id;
-                        $fila ["orden"] = $rotacion->orden;
-			$res [$k] = $fila;
-		}
-		
-		if ($res != null) {
-			// deben devolverse en un echo porque son cadenas de texto
-			echo json_encode ( array (
-					"status" => "ok",
-					"data" => $res,
-					"msg" => "Datos cargados"
-			) );
-		} else {
-			echo json_encode ( array (
-					"status" => "error",
-					"msg" => "error en BD"
-			) );
+			
+			if ($res != null) {
+				// deben devolverse en un echo porque son cadenas de texto
+				echo json_encode ( array (
+						"status" => "ok",
+						"data" => $res,
+						"msg" => "Datos cargados" 
+				) );
+			} else {
+				echo json_encode ( array (
+						"status" => "error",
+						"msg" => "error en BD" 
+				) );
+			}
 		}
 	}
 	public function borrar() {
-		if (isset ( $_REQUEST ["id"] )) {
-			if (! empty ( $_REQUEST ["id"] )) {
-				
-				$status = $this->adaptador_model->delete ( "rotacion", $this->utilphp->sanear ( $_REQUEST ["id"] ) );
-				
-				if ($status) {
-					echo json_encode ( array (
-							"status" => "ok",
-							"msg" => "datos eliminados"
-					) );
+		if ($this->consultarPermisosJuez ()) {
+			if (isset ( $_REQUEST ["id"] )) {
+				if (! empty ( $_REQUEST ["id"] )) {
+					
+					$status = $this->adaptador_model->delete ( "rotacion", $this->utilphp->sanear ( $_REQUEST ["id"] ) );
+					
+					if ($status) {
+						echo json_encode ( array (
+								"status" => "ok",
+								"msg" => "datos eliminados" 
+						) );
+					} else {
+						echo json_encode ( array (
+								"status" => "error",
+								"msg" => "Error al borrar una rotacion " 
+						) );
+					}
 				} else {
 					echo json_encode ( array (
 							"status" => "error",
-							"msg" => "Error al borrar una rotacion "
+							"msg" => "Error algún dato está vacío" 
 					) );
 				}
 			} else {
 				echo json_encode ( array (
 						"status" => "error",
-						"msg" => "Error algún dato está vacío"
+						"msg" => "Error no han llegado los datos" 
 				) );
 			}
-		} else {
-			echo json_encode ( array (
-					"status" => "error",
-					"msg" => "Error no han llegado los datos"
-			) );
 		}
 	}
 }
